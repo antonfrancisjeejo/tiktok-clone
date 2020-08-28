32f7111ec1ddeb85741d9b3b8c704e1a06961e5e
unknown <anton13jeejo@gmail.com>
2020-08-28 18:35:25 +0530

    Initial commit

---
+++ src/components/Video/Video.js
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from "react";
+import "./Video.css";
+import VideoFooter from "../VideoFooter/VideoFooter";
+import VideoSidebar from "../VideoSidebar/VideoSidebar";
+
+const Video = ({
+  url,
+  channel,
+  description,
+  song,
+  likes,
+  messages,
+  shares,
+}) => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+  return (
+    <div className="video">
+      <video
+        className="video_player"
+        loop
+        onClick={onVideoPress}
+        ref={videoRef}
+        src={url}
+      ></video>
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} messages={messages} shares={shares} />
+    </div>
+  );
+};
+
+export default Video;
